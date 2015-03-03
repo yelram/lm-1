21a1f94e8f38132bc2d2649126d7a14285b694f2
yelram <marleykafato@sympatico.ca>
2015-03-02 19:20:08 -0500

    Create static pages: home, about, contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  root 'static_pages#home'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
